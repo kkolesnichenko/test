b569115a2562f39537fe8ccc03a55a2f21c0e19e
kkolesnichenko <36415980+kkolesnichenko@users.noreply.github.com>
2019-07-25 23:07:30 +0300

    25.07
    Задания по canvas
    8 задач на меню

--- canvas.js
+++ canvas.js
@@ -25,18 +25,86 @@ ctx4.lineTo(100,100);
 ctx4.stroke();
 
 var sqr1=cfirst.getContext('2d');
-sqr1.moveTo(60,30);
-sqr1.lineTo(80,30);
-sqr1.stroke();
-var sqr2=cfirst.getContext('2d');
-sqr2.moveTo(80,30);
-sqr2.lineTo(80,50);
-sqr2.stroke();
-var sqr3=cfirst.getContext('2d');
-sqr3.moveTo(80,50);
-sqr3.lineTo(60,50);
-sqr3.stroke();
-var sqr4=cfirst.getContext('2d');
-sqr4.moveTo(60,50);
-sqr4.lineTo(60,30);
-sqr4.stroke();
+sqr1.strokeRect(60,30,30,30);
+
+var trgl1=cfirst.getContext('2d');
+trgl1.moveTo(150,80);
+trgl1.lineTo(180,50);
+trgl1.stroke();
+var trgl2=cfirst.getContext('2d');
+trgl2.moveTo(180,50);
+trgl2.lineTo(200,80);
+trgl2.stroke();
+var trgl3=cfirst.getContext('2d');
+trgl3.moveTo(200,80);
+trgl3.lineTo(150,80);
+trgl3.stroke();
+
+var line1=cfirst.getContext('2d');
+line1.moveTo(20,100);
+line1.lineTo(80,100);
+line1.stroke();
+var line2=cfirst.getContext('2d');
+line2.moveTo(60,80);
+line2.lineTo(80,100);
+line2.stroke();
+var line3=cfirst.getContext('2d');
+line3.moveTo(60,120);
+line3.lineTo(80,100);
+line3.stroke();
+
+var blrect=cfirst.getContext('2d');
+blrect.rect(30,120,60,40);
+blrect.fillStyle='blue';
+blrect.fill();
+
+var rcl=cfirst.getContext('2d');
+rcl.fillRect(150,150,40,50);
+rcl.clearRect(155,160,20,15);
+
+function getRadians(a){
+    return (Math.PI/180)*a;
+}
+var circle=cfirst.getContext('2d');
+circle.arc(25,150,75,0,getRadians(180));
+circle.stroke();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
